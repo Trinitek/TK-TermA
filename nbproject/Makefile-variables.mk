e2f78e0cb708a9000fda6efa5378ac08689c7ad0
Blake Burgess <trinitek@live.com>
2015-02-28 19:20:44 -0500

    Initial commit

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=TK-TermA.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/TK-TermA.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=tk-terma.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/tk-terma.x.tar
